076e48d37f890a7120f53448a05f73bffb368625
Hovsep200645 <hmos3891@gmail.com>
2025-10-11 13:14:26 +0400

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,6 @@
+sudo pacman -S pulseaudio pavucontrol firefox telegram-desktop mousepad gimp inkscape  
+blender ghostscript obs-studio xdg-desktop-portal-wlr transmission-gtk python  
+imv mpv nemo waybar grim slurp swaybg swaylock mako jq wofi htop cmus neofetch ranger unzip
+ttf-nerd-fonts-symbols
+
+yay cava
