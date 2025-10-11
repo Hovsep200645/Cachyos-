e850279b109e85b277b01e177bf53972f0cbf60b
Hovsep200645 <hmos3891@gmail.com>
2025-10-11 13:20:36 +0400

    Create 2.sh

---
+++ 2.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/hyprwm/hyprpaper  
+cd hyprpaper  
+make all  
+  
+sudo cp ~/hyprpaper/build/hyprpaper /usr/bin 
+
+Ну или из реп поставить sudo pacman -S hyprpaper или из аура
